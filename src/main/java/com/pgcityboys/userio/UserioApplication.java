13a60feebbe122c81c18fb8496f40cde6d5ff5f9
lukrzak <lukrzak@wp.pl>
2024-06-15 13:32:47 +0200

    Initial commit

---
+++ src/main/java/com/pgcityboys/userio/UserioApplication.java
@@ -0,0 +1,13 @@
+package com.pgcityboys.userio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pgcityboys/userio/UserioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgcityboys.userio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
