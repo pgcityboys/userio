b91948720d5d28e2104b663a5b737f8713d69737
lukrzak <lukrzak@wp.pl>
2024-06-16 07:45:36 +0200

    env-fix

--- src/test/java/com/pgcityboys/userio/UserioApplicationTests.java
+++ src/test/java/com/pgcityboys/userio/UserioApplicationTests.java
@@ -3,11 +3,6 @@ package com.pgcityboys.userio;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class UserioApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
